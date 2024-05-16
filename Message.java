fcad08eb92aee26ef540e92e862fde21c9abbf0d
mohammedkhadhar49 <127741511+mohammedkhadhar49@users.noreply.github.com>
2024-05-16 22:12:07 +0530

    Update Message.java

--- Message.java
+++ Message.java
@@ -1,5 +1,6 @@
  class Message{
  public static void main(String args[]){
 	 System.out.println("this is sample jenkins message from git");
+	 System.out.println("by using trigger automatically commit the changes......");
  }
 }
