707986f244a6b6525edbad2e42f8ac8701ddbe85
mohammedkhadhar49 <127741511+mohammedkhadhar49@users.noreply.github.com>
2024-05-22 00:01:30 +0530

    Update Message.java

--- Message.java
+++ Message.java
@@ -2,5 +2,6 @@
  public static void main(String args[]){
 	 System.out.println("this is sample jenkins message from git");
 	 System.out.println("by using trigger automatically commit the changes......");
+	  System.out.println("this is crone job after commit......");
  }
 }
